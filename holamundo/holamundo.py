d5eb5c786c791bbc81d0f0accd421ad6a5a8f596
Pablo Luque <pablols114@gmail.com>
2021-10-05 09:55:53 +0200

    Arreglada concatenacion

--- holamundo/holamundo.py
+++ holamundo/holamundo.py
@@ -85,9 +85,9 @@ class HolaMundo(commands.Cog):
             weather_description = z[0]["description"]
 
             embed = discord.Embed(color=0x2ecc71, title='Resumen del dia')
-            embed.add_field(name='Temperatura:', value=(current_temperature + "°"))
-            embed.add_field(name='Presion atmosferica:', value=(current_pressure + "hPa"))
-            embed.add_field(name='Humedad:', value=(current_humidity+"%"))
+            embed.add_field(name='Temperatura:', value='{current_temperature} + °')
+            embed.add_field(name='Presion atmosferica:', value='{current_pressure} + hPa')
+            embed.add_field(name='Humedad:', value='{current_humidity} + %')
             embed.add_field(name='Descripcion:', value=weather_description)
             embed.set_footer(text='Creado por Fallen')   
             await ctx.send(embed=embed)
