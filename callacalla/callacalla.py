c6d3ae47413b2a96fc82cd9a02719f4c51cbca90
Duva-01 <martdiaz01.d@gmail.com>
2023-12-21 19:21:52 +0100

    Añado mi probabilidad al callacalla

--- callacalla/callacalla.py
+++ callacalla/callacalla.py
@@ -1,24 +1,19 @@
-from re import T
+from random import random
 import discord
-import asyncio
 from redbot.core import commands
-from redbot.core.bot import Red
 from discord.utils import get
 from redbot.core.utils.predicates import ReactionPredicate
 from redbot.core.utils.menus import start_adding_reactions
-from random import randint
-
-import json
 
 class CallaCalla(commands.Cog):
     def __init__(self, bot):
-        self.bot: Red = bot
+        self.bot = bot
 
     @commands.hybrid_command(name="callacalla")
     @commands.bot_has_permissions(embed_links=True)
     @commands.guild_only()
     @discord.app_commands.describe(
-        member = "el miembro al que desafiar",
+        member="el miembro al que desafiar",
     )
     async def callacalla(self, ctx: commands.Context, member: discord.Member) -> None:
         """
@@ -37,15 +32,20 @@ class CallaCalla(commands.Cog):
         
         pred = ReactionPredicate.yes_or_no(sent_embed, member)
 
-        # await ctx.bot.wait_for("reaction_add", check=pred, timeout=30)
         await ctx.bot.wait_for("reaction_add", check=pred)
+        
         if pred.result is True:
-            value = randint(0,1)
-            if value == 0:
+            # Aquí verificamos si el autor es tu usuario con una probabilidad del 80% de ganar
+            if ctx.author.id == 402091707937849345:  # Reemplaza TU_ID_DE_DISCORD con tu ID de Discord real
+                value = random() < 0.8
+            else:
+                value = random() < 0.5  # 50% de probabilidad para cualquier otro usuario
+
+            if value:
                 calla_embed.add_field(name="Ganador:", value=f"{ctx.author.mention}", inline=False)
                 calla_embed.add_field(name="A chuparla:", value=f"{member.mention}", inline=False)
                 perdedor = member
-            else :
+            else:
                 calla_embed.add_field(name="Ganador:", value=f"{member.mention}", inline=False)
                 calla_embed.add_field(name="A chuparla", value=f"{ctx.author.mention}", inline=False)
                 perdedor = ctx.author
@@ -63,4 +63,4 @@ class CallaCalla(commands.Cog):
 
         else:
             calla_embed.add_field(name="No ha habido juego:", value=f"{member.mention} ha sido un cobarde", inline=False)
-            await sent_embed.edit(calla_embed)
+            await sent_embed.edit(embed=calla_embed)
